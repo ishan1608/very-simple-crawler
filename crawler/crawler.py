8a1d8f1d97aabaf14e107154d1850b049dc6ff14
Ishan <ishan1608@gmail.com>
2018-12-02 09:34:17 +0530

    added comments explaining the code

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -26,8 +26,9 @@ class Crawler(object):
 
     def execute(self):
         """
-        Call execute() to start crawling
-        :return:
+        Call execute() to start crawling.
+        When done calls `done(result)` with the result as dictionary
+        :return: None
         """
         if self.VERBOSE:
             print('Crawling started. Seed URL: {}'.format(self.seed_url))
@@ -44,9 +45,11 @@ class Crawler(object):
         raise NotImplementedError('You must provide an implementation for done()')
 
     def _crawl_url(self, page_url, depth):
+        # Store traversed URLs
         self._parsed_urls.append(page_url)
         self._url_count += 1
 
+        # Get page contents
         try:
             if self.VERBOSE:
                 print('{} Crawling url: {}'.format(self._url_count, page_url))
@@ -61,18 +64,19 @@ class Crawler(object):
                 'error': error_message
             }
 
+        # Store html in a file
         file_name = 'html/page-{}.html'.format(self._url_count)
         file = open(file_name, 'wb')
         file.write(content)
         file.close()
 
-        base_url = self._get_base_url(page_url)
-
+        # Dictionary to contain this Page and it's children's information
         url_store = {
             'url': page_url,
             'file': file_name,
             'links': []
         }
+        base_url = self._get_base_url(page_url)
 
         dom = lxml.html.fromstring(content)
         links = dom.xpath('//a/@href')
@@ -84,26 +88,38 @@ class Crawler(object):
         return url_store
 
     def _process_link(self, link, depth, base_url):
+        # Clean up link by removing #<anything> and attaching base_url for relative urls
         link = link.split('#')[0]
         link = base_url + link if (link.startswith('/') or link == '') else link
+
+        # Check if link has already been crawled
         if link in self._parsed_urls:
             return
 
+        # Check if maximum depth has reached
         if depth >= self.MAX_DEPTH:
             return {
                 'url': link,
                 'status': 'Maximum depth({}) reached'.format(depth)
             }
 
+        # Check if maximum urls have been crawled
         if self._url_count >= self.MAX_URLS:
             return {
                 'url': link,
                 'status': 'Maximum urls({}) crawled'.format(self._url_count)
             }
 
+        # Crawl the link
         return self._crawl_url(link, depth + 1)
 
     @staticmethod
     def _get_base_url(url):
+        """
+        Utility function to extract base_url from the given url.
+        Ex: Will extract `https://www.wired.com` from `https://www.wired.com/story/how-google-keeps-power-hungry-operations-carbon-neutral/`
+        :param url: url
+        :return: base_url
+        """
         parsed_uri = urlparse(url)
         return '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
